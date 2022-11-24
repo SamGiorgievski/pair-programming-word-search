fda36f8581c3dc3cbccdb8b41d679be199e65602
Samuel Giorgievski <samgiorgievski@gmail.com>
2022-11-23 21:02:47 -0500

    completed assignment

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,96 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+    assert.isTrue(result);
+  });
+
+
+    it("should return true if the word is present even if spelt vertically", function() {
+      const result = wordSearch([
+        ['A', 'W', 'C', 'F', 'Q', 'D', 'A', 'L'],
+        ['S', 'E', 'I', 'N', 'F', 'O', 'L', 'D'],
+        ['Y', 'F', 'C', 'F', 'Q', 'G', 'A', 'L'],
+        ['H', 'M', 'J', 'T', 'E', 'G', 'R', 'G'],
+        ['W', 'H', 'C', 'S', 'Y', 'I', 'R', 'L'],
+        ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+        ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+        ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+        ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+      ], 'DOGGIE');
 
     assert.isTrue(result);
   });
+
+  it("should return false if the word is present even if spelt vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'Q', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'R', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'J', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'C', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'K', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+  assert.isFalse(result);
+});
+
+
+it("should return false if the word is present, but split between rows", function() {
+  const result = wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'D', 'R', 'E'],
+    ['A', 'C', 'T', 'N', 'F', 'O', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'S', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'G', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'R', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'REACT');
+
+assert.isFalse(result);
+});
+
+it("should return false if the word is too long", function() {
+  const result = wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'D', 'R', 'E'],
+    ['A', 'C', 'T', 'N', 'F', 'O', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'S', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'G', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'R', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'MISSISSIPI');
+
+assert.isFalse(result);
+});
+
+it("should return false if the lowercase word is present", function() {
+  const result = wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'seinfeld');
+  assert.isFalse(result);
+});
+
+it("should return false if letter is an empty array", function() {
+  const result = wordSearch([], 'SEINFELD');
+  assert.isFalse(result);
+});
+
+
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,65 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+/*
+* Your project - and a brief description of what it does.
+*/
 
-module.exports = wordSearch
+
+/*
+*
+* Function Name - and a brief description of what it does
+*
+* @param {string} argument name - information it holds
+* @param {string} argument name - information it holds
+*
+*/
+
+
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    //console.log(horizontalJoin);
+    for (let l of horizontalJoin) {
+      //console.log(l);
+      if (l.includes(word)) return true;
+    }
+    //return false;
+  
+    const verticalJoin = [];
+  
+    // loop through the first set of arrays
+    for (let i = 0; i < letters.length; i++) {
+      // loop through each element of each array
+      for (let j = 0; j < letters[i].length; j++) {
+  
+        if (!verticalJoin[j]) {
+          verticalJoin[j] = [];
+        }
+        verticalJoin[j][i] = letters[i][j];
+      }
+    }
+  
+    const verticalJoin2 = verticalJoin.map(arrs => arrs.join(''));
+    console.log(verticalJoin2);
+  
+    for (let x of verticalJoin2) {
+      //console.log(x);
+      if (x.includes(word)) return true;
+    }
+    
+    return false;
+    //console.log(vertiicalJoin2);
+    //console.log(verticalJoin);
+  };
+  
+  wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], "FRANK");
+  
+  
+  module.exports = wordSearch;

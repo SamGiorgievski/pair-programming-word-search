a5d325dd66c8dfdfb6634499c6aed1a0b5f96bf6
Samuel Giorgievski <samgiorgievski@gmail.com>
2022-11-23 21:04:27 -0500

    cleaned up

--- wordsearch.js
+++ wordsearch.js
@@ -15,18 +15,13 @@
 
 const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''));
-    //console.log(horizontalJoin);
     for (let l of horizontalJoin) {
-      //console.log(l);
       if (l.includes(word)) return true;
     }
-    //return false;
   
     const verticalJoin = [];
   
-    // loop through the first set of arrays
     for (let i = 0; i < letters.length; i++) {
-      // loop through each element of each array
       for (let j = 0; j < letters[i].length; j++) {
   
         if (!verticalJoin[j]) {
@@ -37,29 +32,12 @@ const wordSearch = (letters, word) => {
     }
   
     const verticalJoin2 = verticalJoin.map(arrs => arrs.join(''));
-    console.log(verticalJoin2);
   
     for (let x of verticalJoin2) {
-      //console.log(x);
       if (x.includes(word)) return true;
     }
     
     return false;
-    //console.log(vertiicalJoin2);
-    //console.log(verticalJoin);
   };
-  
-  wordSearch([
-    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-  ], "FRANK");
-  
   
   module.exports = wordSearch;
